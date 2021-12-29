8c11b27f70ffc206dab1ea3d2a32400f83db1df9
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-12-29 12:11:03 +0200

    🚀 configure helmet for security

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express, { Application, Request, Response } from 'express'
 import morgan from 'morgan'
+import helmet from 'helmet'
 
 const PORT = 3000
 // create an instance server
 const app: Application = express()
 // HTTP request logger middleware
 app.use(morgan('common'))
+// HTTP security middleware headers
+app.use(helmet())
 // add routing for / path
 app.get('/', (req: Request, res: Response) => {
   res.json({
