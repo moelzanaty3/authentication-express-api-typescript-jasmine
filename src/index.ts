9e3a794166da50779eb711d035770286a61d5af0
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-12-29 11:48:06 +0200

    🚀 start basic server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,17 @@
-console.log('hello world')
+import express, { Application, Request, Response } from 'express'
+
+const PORT = 3000
+// create an instance server
+const app: Application = express()
+
+// add routing for / path
+app.get('/', (req: Request, res: Response) => {
+  res.json({
+    message: 'Hello World 🌍',
+  })
+})
+// start express server
+app.listen(PORT, () => {
+  console.log(`Server is starting at prot:${PORT}`)
+})
+export default app
