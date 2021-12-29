a462985ae02a3efa1db917fdad60a4e1231670c5
mohammedelzanaty <mohammedelzanaty129@gmail.com>
2021-12-29 12:18:00 +0200

    🚀 configure Express Rate Limit middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express'
 import morgan from 'morgan'
 import helmet from 'helmet'
+import rateLimit from 'express-rate-limit'
 
 const PORT = 3000
 // create an instance server
@@ -9,6 +10,18 @@ const app: Application = express()
 app.use(morgan('common'))
 // HTTP security middleware headers
 app.use(helmet())
+// Basic rate-limiting middleware for Express
+// Apply the rate limiting middleware to all requests
+app.use(
+  rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 2, // Limit each IP to 2 requests per `window` (here, per 1 minutes)
+    standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
+    legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+    message: 'el3b b3ed ya ro7 mama',
+  })
+)
+
 // add routing for / path
 app.get('/', (req: Request, res: Response) => {
   res.json({
